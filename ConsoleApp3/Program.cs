8ab06812392cf8e03e792cb0d7797ae1e5439b5b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-18 13:17:03 +0200

    Aggiunta metodo per calcolare sconto e cambio codice programma

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MetodoSconto;
 
 namespace Sconto
 {
@@ -8,25 +9,19 @@ namespace Sconto
         {
             Console.WriteLine("Totale speso? ");
             double totale = double.Parse(Console.ReadLine());
-            double scontoprima;
-            double scontodopo;
             double sconto;
             double scontato;
-            double spesainpiu;
             try
             {
                 if (totale <= 300)
                 {
-                    sconto = totale / 100 * 10;
+                    sconto = MetodoSconto.Sconto.ScontoMinore(totale);
                     scontato = totale - sconto;
                     Console.WriteLine($"Lo sconto è di {sconto} euro e l'importo da pagare è {scontato} euro");
                 }
                 else if (totale > 300)
                 {
-                    scontoprima = 300 / 100 * 10;
-                    spesainpiu = totale - 300;
-                    scontodopo = spesainpiu / 100 * 20;
-                    sconto = scontoprima + scontodopo;
+                    sconto = MetodoSconto.Sconto.ScontoMaggiore(totale);
                     scontato = totale - sconto;
                     Console.WriteLine($"Lo sconto è di {sconto} euro e l'importo da pagare è {scontato} euro");
                 }
---
+++ MetodoSconto/Sconto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MetodoSconto
+{
+    public class Sconto
+    {
+        public static double ScontoMinore(double totale)
+        {
+            double sconto = totale / 100 * 10;
+            return sconto;
+        }
+        public static double ScontoMaggiore(double totale)
+        {
+            double scontoprima = 300 / 100 * 10;
+            double spesainpiu = totale - 300;
+            double scontodopo = spesainpiu / 100 * 20;
+            double sconto = scontoprima + scontodopo;
+            return sconto;
+        }
+    }
+}
